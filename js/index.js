36cd5d62a914f9acebf0bd043221f6a50382539e
CARLOS REINALDO GARCIA NASTAR <kalliche16@gmail.com>
2022-10-13 06:52:06 -0500

    pruebas de coneccion con el servidor

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 
 const app = express();
+const jugadores = [];
 
-//reponder a las peticiones
-app.get("/", (req, res) => {
-    res.send("hola");
+class Jugador {
+  constructor(id) {
+    this.id = id;
+  }
+}
+
+app.get("/unirse", (req, res) => {
+  const id = `${Math.random()}`;
+  const jugador = new Jugador(id);
+  jugadores.push(jugador);
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.send(id);
 });
 
-// arrancar servidor y escuche por un puerto
 app.listen(8080, () => {
-    console.log('Servidor funcionando');
+  console.log("Servidor funcionando");
 });
