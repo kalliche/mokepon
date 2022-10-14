43fea7646e42a2e4bc5a1e87c0848d551ee2ee0c
CARLOS REINALDO GARCIA NASTAR <kalliche16@gmail.com>
2022-10-13 21:44:11 -0500

    coneccion con el backend ok

--- js/code.js
+++ js/code.js
@@ -17,6 +17,7 @@ const sectionVerMapa = document.getElementById("ver-mapa");
 const mapa = document.getElementById("mapa");
 const anchoMaximoDelMapa = 350;
 
+let jugadorId = null;
 let mokepones = [];
 let ataqueJugador = [];
 let ataqueEnemigo = [];
@@ -166,6 +167,7 @@ function unirseAlJuego() {
         res.text()
           .then(function(respuesta) {
             console.log(respuesta)
+            jugadorId = respuesta
           })
       }
     })
@@ -186,9 +188,24 @@ function seleccionarMascotaJugador() {
   } else {
     alert("Selecciona una mascota");
   }
+
+  seleccionarMokepon(mascotaJugador);
+
   extraerAtaques(mascotaJugador);
   sectionVerMapa.style.display = "flex";
   iniciarMapa();
+}
+
+function seleccionarMokepon(mascotaJugador) {
+  fetch(`http://localhost:8080/mokepon/${jugadorId}`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body:JSON.stringify({
+      mokepon: mascotaJugador
+    })
+  });
 }
 
 function extraerAtaques(mascotaJugador) {
